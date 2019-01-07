571c883b03affaacb53965575cc3fdd7d286b9ca
Kushal <kushal.jain63@gmail.com>
2019-01-07 12:46:06 +0530

    1st commit

---
+++ src/components/AppBase.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class AppBase extends Component {
+
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+		<div>
+			Wow, your first project of React is running.
+		</div>
+    );
+  }
+}
+
+export default (AppBase);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppBase from './components/AppBase';
+
+ReactDOM.render(<AppBase/>, document.getElementById('root'));
